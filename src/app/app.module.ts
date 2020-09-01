78f3fdff79f1c92fe9b3d0d6407d2144e1f74592
Bekzod Tolipov <btolipov@Universal.local>
2020-09-01 10:21:12 -0500

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component'
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  bootstrap: [ 
+    AppComponent
+  ]
+})
+export class AppModule { }
